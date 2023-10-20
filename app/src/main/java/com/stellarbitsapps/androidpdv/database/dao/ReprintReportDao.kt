e53cc8d6f8d1db8adff426552d3491ceb63e9df1
StellarBits <stellarbitsapps@gmail.com>
2023-10-20 02:54:19 -0300

    Fixing some warnings

--- app/src/main/java/com/stellarbitsapps/androidpdv/database/dao/ReprintReportDao.kt
+++ app/src/main/java/com/stellarbitsapps/androidpdv/database/dao/ReprintReportDao.kt
@@ -2,11 +2,13 @@ package com.stellarbitsapps.androidpdv.database.dao
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
 import com.stellarbitsapps.androidpdv.database.entity.ReprintReport
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ReprintReportDao {
+    @Transaction
     @Query("SELECT r.* FROM report r " +
             "LEFT JOIN Sangria s ON r.id = s.report_id " +
             "LEFT JOIN ReportError e ON r.id = e.report_id " +
